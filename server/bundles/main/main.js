ca658871b25df466e8b906e048481a5592740031
Valeriyan <rebelling.valeriyan@gmail.com>
2016-05-18 00:53:07 +0500

    Исправить audio

--- server/bundles/main/main.js
+++ server/bundles/main/main.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const SOUNDS_COUNT = 3;
+const SOUNDS_FORMATS = ['mp3', 'ogg'];
+
 require('./main.css');
 
 require('file?name=s0.mp3!./sounds/s0.mp3');
 require('file?name=s1.mp3!./sounds/s1.mp3');
 require('file?name=s2.mp3!./sounds/s2.mp3');
+require('file?name=s0.ogg!./sounds/s0.ogg');
+require('file?name=s1.ogg!./sounds/s1.ogg');
+require('file?name=s2.ogg!./sounds/s2.ogg');
 
 require('../../../client/game/game.js');
+
